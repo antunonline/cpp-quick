cf57739aeee5ea113d81b4a600fb4fb8532e4e83
Antun Horvat <mail@antun-horvat.from.hr>
2019-04-03 19:57:16 +0200

    Add cmake examples

--- CMake/setup.cmake
+++ CMake/setup.cmake
@@ -10,3 +10,28 @@ function(MakeLibStatic NAME)
     set_target_properties(${NAME} PROPERTIES COMPILE_FLAGS "-static-libstdc++ -static")
     set_target_properties(${NAME} PROPERTIES BUILD_SHARED_LIBS OFF)
 endfunction(MakeLibStatic)
+
+function(MakeStaticForProduction TARGET)
+    if(EXISTS "/etc/alpine-release")
+        MakeLibStatic("${NAME}")
+    endif()
+endfunction(MakeStaticForProduction)
+
+
+
+FUNCTION(PREPEND var prefix)
+    SET(listVar "")
+    FOREACH(f ${ARGN})
+        LIST(APPEND listVar "${prefix}/${f}")
+    ENDFOREACH(f)
+    SET(${var} "${listVar}" PARENT_SCOPE)
+ENDFUNCTION(PREPEND)
+
+FUNCTION(REGISTER_LIB_IF_EXISTS NAME)
+    SET(LIBPREFIX "/opt/cpp")
+    SET(LIBPATH "${LIBPREFIX}/${NAME}")
+    if (EXISTS "${LIBPATH}" AND IS_DIRECTORY "${LIBPATH}")
+        include_directories(BEFORE "${LIBPATH}/include/")
+        link_directories(BEFORE "${LIBPATH}/lib")
+    endif ()
+ENDFUNCTION(REGISTER_LIB_IF_EXISTS)
